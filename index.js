c109f5efbac38ca0480fe3a3f8eb1bf021d8897a
maxwellium <max@lawyer.com>
2016-12-28 20:10:32 +0100

    dirty checking array

--- index.js
+++ index.js
@@ -79,18 +79,22 @@ function parseBMFResponse( response ) {
     ausgaben: {}
   };
 
-  for (let eingabe of response.lohnsteuer.eingaben[ 0 ].eingabe) {
-    result.eingaben[ eingabe.$.name ] = {
-      value:  eingabe.$.value,
-      status: eingabe.$.status
-    };
+  if ( response.lohnsteuer.eingaben[ 0 ] && response.lohnsteuer.eingaben[ 0 ].eingabe ) {
+    for (let eingabe of response.lohnsteuer.eingaben[ 0 ].eingabe) {
+      result.eingaben[ eingabe.$.name ] = {
+        value:  eingabe.$.value,
+        status: eingabe.$.status
+      };
+    }
   }
 
-  for (let ausgabe of response.lohnsteuer.ausgaben[ 0 ].ausgabe) {
-    result.ausgaben[ ausgabe.$.name ] = {
-      value: ausgabe.$.value,
-      type:  ausgabe.$.type
-    };
+  if ( response.lohnsteuer.ausgaben[ 0 ] && response.lohnsteuer.ausgaben[ 0 ].ausgabe ) {
+    for (let ausgabe of response.lohnsteuer.ausgaben[ 0 ].ausgabe) {
+      result.ausgaben[ ausgabe.$.name ] = {
+        value: ausgabe.$.value,
+        type:  ausgabe.$.type
+      };
+    }
   }
 
   return result;
